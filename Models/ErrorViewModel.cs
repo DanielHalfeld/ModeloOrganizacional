1e63e7c1b8546c55aab7b5d78c43406e2172040a
Gestão-003 <Gesto-003@LAPTOP-LCV5MAQR>
2023-11-28 16:32:38 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModeloOrganizacional.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
